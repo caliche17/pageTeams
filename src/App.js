dbc7e23a27e2a54be8985cae6c71163b880280b9
carlos velasquez <cavelasquez17@gmail.com>
2020-10-20 17:33:01 -0500

    teams react+eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './pageTeam.sass';
 import Teams from "./components/Teams";
-import Equipos from "./components/Equipos";
+import Equipo from "./components/Equipo";
 
 function App() {
-
   return (
     <div className="App">
       <header>
@@ -19,26 +18,19 @@ function App() {
           <p class='line first'>¿Real Madrid? ¿Barcelona? ¿Boca Jr? ¿Bayern? ¿Juventus?
         No, el líder es una sorpresa. <strong>Aqui pronto lo descubriras!!</strong></p>
         </div>
-
         <div class='contenido'>
-          
-          <Equipos description="146 títulos (137 nacionales + 9 internacionales)" nombre="1) NACIONAL (Uruguay)" id ="nacional" imgUrl="https://www.nacional.uy/images/logo-pie.png"/>
-          <Equipos description="127 títulos (119 nacionales + 8 internacionales)" nombre="2) PEÑAROL (Uruguay)" id= "penarol" imgUrl="https://upload.wikimedia.org/wikipedia/commons/2/26/Escudo-club-atletico-penarol.png" />
-          <Equipos description="116 títulos (96 nacionlales + 20 internacionales)" nombre="3) AL AHLY (Egipto)" id="alahly" imgUrl="https://upload.wikimedia.org/wikipedia/en/7/73/%D9%90%D9%90ASC.ALAHLYSC.png" />
-          <Equipos description="115 títulos (114 nacionales + 1 internacional)" nombre="4) RANGERS (Escocia)" id= "rangers" imgUrl="https://1.bp.blogspot.com/-CZogrrxnU7o/XwZkf99vKYI/AAAAAAABezY/nBdEEWuhmucaKEV6cdTGkCtZDoU8Tz3VwCK4BGAsYHg/s512/Rangers%2BFC.png" />
-          
+          <Equipo description="146 títulos (137 nacionales + 9 internacionales)" nombre="1) NACIONAL (Uruguay)" id="nacional" imgUrl="https://www.nacional.uy/images/logo-pie.png" />
+          <Equipo description="127 títulos (119 nacionales + 8 internacionales)" nombre="2) PEÑAROL (Uruguay)" id="penarol" imgUrl="https://upload.wikimedia.org/wikipedia/commons/2/26/Escudo-club-atletico-penarol.png" />
+          <Equipo description="116 títulos (96 nacionlales + 20 internacionales)" nombre="3) AL AHLY (Egipto)" id="alahly" imgUrl="https://upload.wikimedia.org/wikipedia/en/7/73/%D9%90%D9%90ASC.ALAHLYSC.png" />
+          <Equipo description="115 títulos (114 nacionales + 1 internacional)" nombre="4) RANGERS (Escocia)" id="rangers" imgUrl="https://1.bp.blogspot.com/-CZogrrxnU7o/XwZkf99vKYI/AAAAAAABezY/nBdEEWuhmucaKEV6cdTGkCtZDoU8Tz3VwCK4BGAsYHg/s512/Rangers%2BFC.png" />
         </div>
-
       </section>
-
       <h4>Pero estos son los mejores equipos segun la FIFA 2020</h4>
-      <ul id="lista-equipos"> 
+      <ul id="lista-equipos">
+        <Teams />
       </ul>
-      {Teams}
     </div>
-   
   );
-
 }
 
 export default App;
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,21 +1,9 @@
-window.onload =function (){
+import React from 'react';
+export default function () {
 
-const listTeams = [ "liverpool", "flamengo", "barcelona", "river plate", "palmeiras",]
-const contenedor = document.querySelector('#lista-equipos')
+  const listTeams = ["liverpool", "flamengo", "barcelona", "river plate", "palmeiras",]
 
-if (contenedor) {
-    listTeams.forEach(function (text) {
-      const item = document.createElement('li')
-      item.className = 'equipos'
-      const valor = listTeams.indexOf(text) + 1
-      item.id = valor
-      contenedor.appendChild(item)
-      const a = document.createElement('a')
-      a.setAttribute('href', './pagina2.html?equipoId=' + valor)
-      a.textContent = text
-      item.appendChild(a)
-    })
-  }
-};
-
-export default window.onload
+  return (
+    listTeams.map((equipo, index) => <li className="equipos" id={index + 1}><a href={`./pagina2.html?equipoId=${index + 1}`}>{equipo}</a></li>)
+  );
+}
